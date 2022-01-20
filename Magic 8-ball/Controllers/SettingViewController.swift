7344c9d6b01493aef4d7ec49a7be67b584b73379
Vladyslav Taranenko <bra1nf00d@icloud.com>
2022-01-20 02:19:34 +0200

    create Second Screen

---
+++ Magic 8-ball/Controllers/SettingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingViewController.swift
+//  Magic 8-ball
+//
+//  Created by Vladyslav Taranenko on 20.01.2022.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
