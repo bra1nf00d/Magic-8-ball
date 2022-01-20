e97b7c7b2997667724b4ebfdd88090dbe278b698
Vladyslav Taranenko <bra1nf00d@icloud.com>
2022-01-20 02:05:26 +0200

    Initial Commit

---
+++ Magic 8-ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8-ball
+//
+//  Created by Vladyslav Taranenko on 20.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
