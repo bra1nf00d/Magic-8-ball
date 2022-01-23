dbb598ca963a496ed0e870605c2afb2d57d0671a
Vladyslav Taranenko <bra1nf00d@icloud.com>
2022-01-23 16:45:54 +0200

    change UIViewController to a UITableViewController

--- Magic 8-ball/Controllers/SettingViewController.swift
+++ Magic 8-ball/Controllers/SettingViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
