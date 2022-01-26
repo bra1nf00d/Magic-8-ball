ea258f7ea76f55c300f9afb881e674b948774d1a
Vladyslav Taranenko <bra1nf00d@icloud.com>
2022-01-26 13:50:44 +0200

    add datasource	 methods and display cells on screen

--- Magic 8-ball/Controllers/SettingViewController.swift
+++ Magic 8-ball/Controllers/SettingViewController.swift
@@ -9,8 +9,24 @@ import UIKit
 
 class SettingViewController: UITableViewController {
 
+    let answers = ["Just do it", "Change your mind"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    // MARK: - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return answers.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnswerCell", for: indexPath)
+        
+        cell.textLabel?.text = answers[indexPath.row]
+        
+        return cell
     }
 }
