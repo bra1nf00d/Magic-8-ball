d8e321e7fe5bcd7d74c934fa7591b611f0503f73
Vladyslav Taranenko <bra1nf00d@icloud.com>
2022-02-19 17:21:01 +0200

    created swipeable table class

---
+++ Magic 8-ball/Controllers/SwipeTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeTableViewController.swift
+//  Magic 8-ball
+//
+//  Created by Vladyslav Taranenko on 19.02.2022.
+//
+
+import UIKit
+
+class SwipeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+}
